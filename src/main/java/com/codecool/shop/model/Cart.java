79faf777c521ff9b28a6496b24fc4139cdc89a98
kinga00099 <kinga00099@gmail.com>
2018-11-19 13:40:27 +0100

    Cart dao pattern added, yet empty classes+interface

---
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -0,0 +1,11 @@
+package com.codecool.shop.model;
+
+public class Cart extends BaseModel {
+    public Cart(String name) {
+        super(name);
+    }
+
+    public Cart(String name, String description) {
+        super(name, description);
+    }
+}
