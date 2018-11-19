d01ac73977a55b9df1f51af91b85127e620ca1d2
kinga00099 <kinga00099@gmail.com>
2018-11-19 14:02:01 +0100

    dao pattern removed, lineitem class added

---
+++ src/main/java/com/codecool/shop/order/LineItem.java
@@ -0,0 +1,50 @@
+package com.codecool.shop.order;
+
+import com.codecool.shop.model.Product;
+
+public class LineItem {
+
+    private String productName;
+    private double productPrice;
+    private int quantity;
+    private double totalPrice;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+        public void setTotalPrice(int quantity){
+            this.totalPrice = quantity * productPrice;
+        }
+
+        public double getTotalPrice(){
+            return totalPrice;
+        }
+
+    public LineItem(Product product){
+        this.productName = product.getName();
+        this.productPrice = product.getDefaultPrice();
+        this.quantity = 1;
+        this.totalPrice = productPrice;
+    }
+}
