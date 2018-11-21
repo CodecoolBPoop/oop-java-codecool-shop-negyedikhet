e2b351316b54d71c6fb207ab824d2b1b1c8af485
TribTrob <trib073@gmail.com>
2018-11-21 14:44:37 +0100

    Modal now displays the total amount to be played

--- src/main/webapp/static/js/cartReview.js
+++ src/main/webapp/static/js/cartReview.js
@@ -15,6 +15,7 @@ btn.onclick = function () {
         }, function(response, status) {
             if (status === "success"){
                 console.log(response);
+                let list_num = 0;
                 while(tbody.firstChild) tbody.removeChild(tbody.firstChild);
                 for(let i=0; i < response.length; i++){
                     let new_table_row = document.createElement("tr");
@@ -25,7 +26,14 @@ btn.onclick = function () {
                         new_table_row.appendChild(new_table_cell);
                         tbody.appendChild(new_table_row);
                     }
+                    list_num += parseInt(response[i]["totalPrice"]);
+                    console.log(list_num)
+
                 }
+                let total_price = document.createTextNode(list_num + "USD");
+                let new_list_element = document.createElement("li");
+                new_list_element.appendChild(total_price);
+                tbody.appendChild(new_list_element);
             }
         }
     )
