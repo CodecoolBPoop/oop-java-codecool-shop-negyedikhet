3d6bb9f5ba41712e0ad3aa2acae161f3a0943f44
dr1zzt2 <dr1zzt2.NK@gmail.com>
2018-11-21 12:30:47 +0100

    sorting finished with js

--- src/main/webapp/static/js/sorting.js
+++ src/main/webapp/static/js/sorting.js
@@ -1,7 +1,26 @@
 function sortingSetup() {
-    let sortingSelection = document.getElementById("selectioncategory");
-    sortingSelection.addEventListener("change", function() {
-        alert();
+    const urlParams = new URLSearchParams(window.location.search);
+    const categoryParam = urlParams.get('categoryID');
+    const supplierParam = urlParams.get('supplierID');
+    let sortingSelectionCat = document.getElementById("selectioncategory");
+    let sortingSelectionSup = document.getElementById("selectionsupplier");
+    if (categoryParam != null) {
+        sortingSelectionCat.options[categoryParam-1].selected = 'selected';
+    }
+    if (supplierParam != null) {
+        sortingSelectionSup.options[supplierParam-1].selected = 'selected';
+    }
+
+
+    sortingSelectionCat.addEventListener("change", function() {
+        let categoryID = this.options[this.selectedIndex].dataset.categoryid;
+        urlParams.set("categoryID", categoryID);
+        window.location.assign("?" + urlParams.toString());
+    });
+    sortingSelectionSup.addEventListener("change", function() {
+        let supplierID = this.options[this.selectedIndex].dataset.supplierid;
+        urlParams.set("supplierID", supplierID);
+        window.location.assign("?" + urlParams.toString());
     });
 }
 
