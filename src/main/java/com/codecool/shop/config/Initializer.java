fa94ad427f5f6e6f22cb17952d545e0f0ee06915
kinga00099 <kinga00099@gmail.com>
2018-11-22 12:53:35 +0100

    Webshop filled with christmas products, main page design started

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -24,21 +24,54 @@ public class Initializer implements ServletContextListener {
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
         //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
+        Supplier lipoti = new Supplier("Lipoti", "Freshly baked products");
+        supplierDataStore.add(lipoti);
+        Supplier gucci = new Supplier("Gucci", "Clothing");
+        supplierDataStore.add(gucci);
+        Supplier surefire = new Supplier("Surefire", "Christmas decorations");
+        supplierDataStore.add(surefire);
+        Supplier milka = new Supplier("Milka", "Christmas chocolates");
+        supplierDataStore.add(milka);
+        Supplier rudolf = new Supplier("Rudolf", "Artificial christmas trees");
+        supplierDataStore.add(rudolf);
+        Supplier ginger = new Supplier("GingerFactory", "Christmas cookies");
+        supplierDataStore.add(ginger);
+
 
         //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
-        ProductCategory boi = new ProductCategory("Boi", "Software", "Boiboi");
-        productCategoryDataStore.add(boi);
+        ProductCategory clothes = new ProductCategory("Clothes", "Clothes", "Festive sweaters.");
+        productCategoryDataStore.add(clothes);
+        ProductCategory cookie = new ProductCategory("Cookie", "Food", "Finest freshly baked christmas cookies.");
+        productCategoryDataStore.add(cookie);
+        ProductCategory decor = new ProductCategory("Decoration", "Decoration", "Small christmas decorations");
+        productCategoryDataStore.add(decor);
+        ProductCategory chocolatecandy = new ProductCategory("ChocolateCandy", "Food", "Hungarian Christmas-candy speciality");
+        productCategoryDataStore.add(chocolatecandy);
+        ProductCategory tree = new ProductCategory("Christmas Tree", "Decoration", "Christmas trees");
+        productCategoryDataStore.add(tree);
+
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("Boiprasent", 4, "USD", "Boiprasent gut priceyy", boi, amazon));
+        productDataStore.add(new Product("Sweater with bow", 25, "USD", "Beautiful gift for your loved ones.", clothes, lipoti));
+        productDataStore.add(new Product("Star Wars sweater", 32, "USD", "The perfect present for Star Wars fans. ", clothes, lipoti));
+        productDataStore.add(new Product("Kitten sweater", 39, "USD", "How else could you surprise your beloved girlfriend?", clothes, lipoti));
+        productDataStore.add(new Product("Gingerbread - Tree", 9, "USD", "Finest freshly baked gingerbread.", cookie, ginger));
+        productDataStore.add(new Product("Gingerbread - Classic", 2, "USD", "Classic gingerbread figure.", cookie, ginger));
+        productDataStore.add(new Product("Gingerbread - House", 19, "USD", "Handmade gingerbread house.", cookie, ginger));
+        productDataStore.add(new Product("Christmas Bejgli", 9, "USD", "Walnut flavoured hungarian cookie speciality.", cookie, ginger));
+        productDataStore.add(new Product("Xmas Ball - Red", 1, "USD", "Shiny red christmas decoration ball", decor, surefire));
+        productDataStore.add(new Product("Xmas Ball - Blue", 1, "USD", "Shiny blue christmas decoration ball", decor, surefire));
+        productDataStore.add(new Product("Xmas Ball - Purple", 1, "USD", "Shiny purple christmas decoration ball", decor, surefire));
+        productDataStore.add(new Product("Wreath - Simple", 19, "USD", "Simple, but beautiful Christmas wreath", decor, surefire));
+        productDataStore.add(new Product("Wreath - Classic", 25, "USD", "Classic, decorated Christmas wreath", decor, surefire));
+        productDataStore.add(new Product("Wreath - Silver", 39, "USD", "Special silver colored Christmas wreath ", decor, surefire));
+        productDataStore.add(new Product("Xmas candy - Marzipan ", 9, "USD", "Marzipan flavoured hungarian choco-candy speciality.", chocolatecandy, milka));
+        productDataStore.add(new Product("Xmas candy - Selection", 15, "USD", "Selection of hungarian choco-candy specialities.", chocolatecandy, milka));
+        productDataStore.add(new Product("Xmas candy - Nut", 9, "USD", "Nut flavoured hungarian choco-candy speciality.", chocolatecandy, milka));
+        productDataStore.add(new Product("Xmas Tree - Silver", 59, "USD", "Silver colored Christmas tree", tree, rudolf));
+        productDataStore.add(new Product("Xmas Tree - Soft", 49, "USD", "Christmas tree with especially soft leaves", tree, rudolf));
+        productDataStore.add(new Product("Xmas Tree - Forest", 79, "USD", "Forest Christmas tree", tree, rudolf));
+        productDataStore.add(new Product("Xmas Tree - Classic", 39, "USD", "Classic Christmas tree", tree, rudolf));
+
     }
 }
