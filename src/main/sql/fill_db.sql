4d4ff50a49463f172dccda430b00eb0586ca1994
kinga00099 <kinga00099@gmail.com>
2018-12-03 15:15:31 +0100

    fill db with suppliers prepared

---
+++ src/main/sql/fill_db.sql
@@ -0,0 +1,7 @@
+INSERT INTO suppliers (name, description)
+VALUES ('All', 'All supplier'),
+('Gucci', 'Clothing'),
+('Lipoti', 'Freshly baked products'),
+('Surefire','Christmas decorations'),
+('Milka','Christmas chocolates'),
+('Rudolf','Artificial christmas trees');
