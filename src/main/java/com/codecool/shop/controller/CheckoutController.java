d62cd5aafec5d2e239ab8984da6024d2b7070d83
dr1zzt2 <dr1zzt2.NK@gmail.com>
2018-12-05 13:39:36 +0100

    checkout and payment changes for using Stripe

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -23,7 +23,11 @@ public class CheckoutController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+        HttpSession mySession = req.getSession();
+        Order myOrder = (Order) mySession.getAttribute("order");
         context.setVariable("recipient", "World");
+        context.setVariable("description", "Jingling Webshop");
+        context.setVariable("amount", myOrder.getCartTotalPrice());
         engine.process("checkout.html", context, resp.getWriter());
     }
 
--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -31,7 +31,6 @@ function init () {
         inputName.setAttribute("required", "");
 
 
-
         divPass = document.createElement("div");
         divPass.classList.add("form-group");
 
@@ -55,102 +54,6 @@ function init () {
         divPass.appendChild(labelPass);
         divPass.appendChild(inputPass);
         divRow.appendChild(divPass);
-
-        formNode.appendChild(divRow);
-
-
-    } else if (paymentMethod === "creditcard") {
-        let divCardNumber, divCardHolder, divExpDate, divCode;
-        let labelCardNumber, labelCardHolder, labelExpDate, labelCode;
-        let inputCardNumber, inputCardHolder, inputExpdate, inputCode;
-        let spanCreditCard;
-        let divRow;
-        divRow = document.createElement("div");
-        divRow.classList.add("form-row");
-
-        spanCreditCard = document.createElement("span");
-        spanCreditCard.classList.add("dataParagraph");
-        spanCreditCard.innerHTML = "Credit Card Information";
-
-       divCardNumber = document.createElement("div");
-        divCardNumber.classList.add("form-group");
-
-        labelCardNumber = document.createElement("label");
-        labelCardNumber.setAttribute("for", "cardnumber");
-        labelCardNumber.classList.add("formLabels");
-        labelCardNumber.innerHTML = "Card Number";
-
-        inputCardNumber = document.createElement("input");
-        inputCardNumber.setAttribute("type", "text");
-        inputCardNumber.classList.add("form-control");
-        inputCardNumber.setAttribute("id", "cardnumber");
-        inputCardNumber.setAttribute("placeholder", "Card Number");
-        inputCardNumber.setAttribute("required", "");
-
-
-        divCardHolder = document.createElement("div");
-        divCardHolder.classList.add("form-group");
-
-        labelCardHolder = document.createElement("label");
-        labelCardHolder.setAttribute("for", "cardholder");
-        labelCardHolder.classList.add("formLabels");
-        labelCardHolder.innerHTML = "Card Holder";
-
-        inputCardHolder = document.createElement("input");
-        inputCardHolder.setAttribute("type", "text");
-        inputCardHolder.classList.add("form-control");
-        inputCardHolder.setAttribute("id", "cardholder");
-        inputCardHolder.setAttribute("placeholder", "Card Holder");
-        inputCardHolder.setAttribute("required", "");
-
-
-        divExpDate = document.createElement("div");
-        divExpDate.classList.add("form-group");
-
-        labelExpDate = document.createElement("label");
-        labelExpDate.setAttribute("for", "cardnumber");
-        labelExpDate.classList.add("formLabels");
-        labelExpDate.innerHTML = "Expiration Date";
-
-        inputExpdate = document.createElement("input");
-        inputExpdate.setAttribute("type", "text");
-        inputExpdate.classList.add("form-control");
-        inputExpdate.setAttribute("id", "expdate");
-        inputExpdate.setAttribute("placeholder", "Expiration Date");
-        inputExpdate.setAttribute("required", "");
-
-
-        divCode = document.createElement("div");
-        divCode.classList.add("form-group");
-
-        labelCode = document.createElement("label");
-        labelCode.setAttribute("for", "code");
-        labelCode.classList.add("formLabels");
-        labelCode.innerHTML = "Code";
-
-        inputCode = document.createElement("input");
-        inputCode.setAttribute("type", "text");
-        inputCode.classList.add("form-control");
-        inputCode.setAttribute("id", "code");
-        inputCode.setAttribute("placeholder", "Code");
-        inputCode.setAttribute("required", "");
-
-        formNode.appendChild(spanCreditCard);
-        divCardNumber.appendChild(labelCardNumber);
-        divCardNumber.appendChild(inputCardNumber);
-        divRow.appendChild(divCardNumber);
-
-        divCardHolder.appendChild(labelCardHolder);
-        divCardHolder.appendChild(inputCardHolder);
-        divRow.appendChild(divCardHolder);
-
-        divExpDate.appendChild(labelExpDate);
-        divExpDate.appendChild(inputExpdate);
-        divRow.appendChild(divExpDate);
-
-        divCode.appendChild(labelCode);
-        divCode.appendChild(inputCode);
-        divRow.appendChild(divCode);
 
         formNode.appendChild(divRow);
     }
