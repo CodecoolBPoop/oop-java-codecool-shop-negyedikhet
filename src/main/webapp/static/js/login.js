c883032688f6f7a6484c48d09693949949414a09
kinga00099 <kinga00099@gmail.com>
2019-01-03 15:55:17 +0100

    Modal now disappear when successfully registered/logged in

--- src/main/webapp/static/js/login.js
+++ src/main/webapp/static/js/login.js
@@ -5,6 +5,7 @@ function init() {
 function login() {
     let loginButton = document.getElementById("loginButton");
     loginButton.addEventListener("click", function () {
+        let loginResponse = document.getElementById("loginResponse");
         let email = document.getElementById("loginEmail").value;
         let password = document.getElementById("loginPassword").value;
         let url = "signin";
@@ -12,8 +13,14 @@ function login() {
             {
                 "email": email,
                 "password": password,
-            }
-        );
+            }).done(function () {
+            loginResponse.innerText = "Succesfully logged in";
+            setTimeout(function () {
+                $("#loginModal").modal('hide');
+            }, 1000);
+        }).fail(function () {
+            loginResponse.innerText = "Email or username is invalid!"
+        })
     })
 }
 
--- src/main/webapp/static/js/register.js
+++ src/main/webapp/static/js/register.js
@@ -9,13 +9,15 @@ function register() {
             let email = document.getElementById("regEmail").value;
             let password = document.getElementById("regPassword").value;
             let url = "signup";
-
             $.post(url,
                 {
                     "email": email,
                     "password": password,
                 }).done(function () {
-                regResponse.innerText = "Succesfully registered"
+                regResponse.innerText = "Succesfully registered";
+                setTimeout(function () {
+                    $("#registerModal").modal('hide');
+                }, 1000);
             }).fail(function () {
                 regResponse.innerText = "This user is already registered!"
             })
