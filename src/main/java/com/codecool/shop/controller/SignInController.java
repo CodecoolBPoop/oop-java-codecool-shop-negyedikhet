0063a7df5ddcf81c37affbc447448a49c6ef9aba
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-01-03 14:31:05 +0100

    fincsi user implementation

---
+++ src/main/java/com/codecool/shop/controller/SignInController.java
@@ -0,0 +1,31 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.implementation.UserDaoJdbc;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/signin"})
+
+public class SignInController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserDaoJdbc userDataStore = UserDaoJdbc.getInstance();
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
+        if (BCrypt.checkpw(userDataStore.find(email).getPassword(), hashed)) {
+            System.out.println("It matches");
+            HttpSession mySession = req.getSession();
+            mySession.setAttribute("username", email);
+        }  else {
+            System.out.println("It does not match");
+        }
+    }
+}
